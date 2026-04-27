f90fc289f47306e77b0ff24724590ffc5d57ca65
Justin Barnett <barnett.justin.p@gmail.com>
2026-04-27 19:27:33 -0400

    Removed unused entity providers barrel
    Result: {"status":"keep","loc":11004,"eslint_scoped":1,"vitest":107}

--- src/entities/providers/index.ts
+++
@@ -1,8 +0,0 @@
-export { WorldDataProvider } from '../index';
-export { MarkdownProvider } from './markdown';
-export { SRDProvider } from './srd';
-export { KankaProvider } from './kanka';
-export { HomebreweryProvider } from './homebrewery';
-export { NotionProvider } from './notion';
-export { GoogleDocsProvider } from './google-docs';
-export { FileUploadProvider } from './file-upload';
