d5f592794616b172ce0694099713c549953a5386
Justin Barnett <barnett.justin.p@gmail.com>
2026-04-27 20:02:24 -0400

    Compacted settings section prop interfaces
    Result: {"status":"keep","loc":10296,"eslint_scoped":1,"vitest":107}

--- src/settings/types.ts
+++ src/settings/types.ts
@@ -4,49 +4,12 @@ import { STTSettings } from '../stt/index';
 
 interface StyledSectionProps { styles: any }
 
-export interface DisplaySectionProps extends StyledSectionProps {
-  cardSize: import('../context/ui-settings').CardSize;
-  setCardSize: (size: import('../context/ui-settings').CardSize) => void;
-  colorScheme: import('../context/ui-settings').ColorScheme;
-  setColorScheme: (scheme: import('../context/ui-settings').ColorScheme) => void;
-}
+export interface DisplaySectionProps extends StyledSectionProps { cardSize: import('../context/ui-settings').CardSize; setCardSize: (size: import('../context/ui-settings').CardSize) => void; colorScheme: import('../context/ui-settings').ColorScheme; setColorScheme: (scheme: import('../context/ui-settings').ColorScheme) => void }
 
-export interface VoiceSectionProps extends StyledSectionProps {
-  sttSettings: STTSettings;
-  setSttSettings: React.Dispatch<React.SetStateAction<STTSettings>>;
-  saveVoice: () => Promise<void>;
-  voiceSaved: boolean;
-  isWebSpeech: boolean;
-}
+export interface VoiceSectionProps extends StyledSectionProps { sttSettings: STTSettings; setSttSettings: React.Dispatch<React.SetStateAction<STTSettings>>; saveVoice: () => Promise<void>; voiceSaved: boolean; isWebSpeech: boolean }
 
-export interface DataSectionProps extends StyledSectionProps {
-  dsLocal: DataSourcesSettings;
-  setDsLocal: React.Dispatch<React.SetStateAction<DataSourcesSettings>>;
-  saveData: () => Promise<void>;
-  dataSaved: boolean;
-}
+export interface DataSectionProps extends StyledSectionProps { dsLocal: DataSourcesSettings; setDsLocal: React.Dispatch<React.SetStateAction<DataSourcesSettings>>; saveData: () => Promise<void>; dataSaved: boolean }
 
-export interface FilesSectionProps extends StyledSectionProps {
-  uploads: UploadedFile[];
-  removingUploadId: string | null;
-  pasteFileName: string;
-  setPasteFileName: React.Dispatch<React.SetStateAction<string>>;
-  pasteContent: string;
-  setPasteContent: React.Dispatch<React.SetStateAction<string>>;
-  pickFilesWeb: () => void;
-  handlePasteAdd: () => Promise<void>;
-  handleDeleteUpload: (id: string) => Promise<void>;
-  handleDeleteAllData: () => Promise<void>;
-  deleteAllPending: boolean;
-  deleteAllStatus: string;
-}
+export interface FilesSectionProps extends StyledSectionProps { uploads: UploadedFile[]; removingUploadId: string | null; pasteFileName: string; setPasteFileName: React.Dispatch<React.SetStateAction<string>>; pasteContent: string; setPasteContent: React.Dispatch<React.SetStateAction<string>>; pickFilesWeb: () => void; handlePasteAdd: () => Promise<void>; handleDeleteUpload: (id: string) => Promise<void>; handleDeleteAllData: () => Promise<void>; deleteAllPending: boolean; deleteAllStatus: string }
 
-export interface AISectionProps extends StyledSectionProps {
-  dsLocal: DataSourcesSettings;
-  setDsLocal: React.Dispatch<React.SetStateAction<DataSourcesSettings>>;
-  aiContent: string;
-  setAiContent: React.Dispatch<React.SetStateAction<string>>;
-  aiParsing: boolean;
-  aiResult: string;
-  handleAIParse: () => Promise<void>;
-}
+export interface AISectionProps extends StyledSectionProps { dsLocal: DataSourcesSettings; setDsLocal: React.Dispatch<React.SetStateAction<DataSourcesSettings>>; aiContent: string; setAiContent: React.Dispatch<React.SetStateAction<string>>; aiParsing: boolean; aiResult: string; handleAIParse: () => Promise<void> }
